bfdb7ed7d21a810dc469d88311cd90caffdc2676
Jonathan Bateson <j.carr.bateson@gmail.com>
2023-10-14 19:06:07 -0400

    Average Change

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,15 +2,18 @@ import os
 import csv
 
 CSV_PATH = os.path.join("Resources", "budget_data.csv")
+MONTH_IDX = 0
+PROFIT_IDX = 1
+
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 total_months = 0 
-total_net = 0
-average_change = 0
+total_profit = 0
+total_change = 0
 maximum_profit = 0
 decrease_profit = 0
-
+is_first_row = True
 # print(os.getcwd())
 # print(__file__)
 
@@ -21,14 +24,25 @@ with open(CSV_PATH) as csv_file:
     header = next(csv_reader)
     
     for row in csv_reader:
-        total_months = total_months + 1
-        total_net = total_net + int(row[1])
+        curr_profit = int(row[PROFIT_IDX])
+        total_months += 1
+        total_profit += curr_profit
+        if is_first_row: 
+            is_first_row = False
+        else:
+            curr_change = curr_profit - prev_profit
+            total_change += curr_change
+    
+        # Prepare for next month
+        prev_profit = curr_profit
+
+average_change = total_change / (total_months - 1)
        
         
-    print("Financial Analysis")
-    print("------------------------")
-    print("Total Months:", total_months)
-    print("Total:", "$",total_net)
-    print("Average Change:", "$")
-    print("Greatest Increase in Profits: ", maximum_profit)
-    print("Greatest Decrease in Profits:", decrease_profit)
+print("Financial Analysis")
+print("------------------------")
+print("Total Months:", total_months)
+print("Total:", "$",total_profit)
+print("Average Change:", f"${average_change}")
+print("Greatest Increase in Profits: ", maximum_profit)
+print("Greatest Decrease in Profits:", decrease_profit)
